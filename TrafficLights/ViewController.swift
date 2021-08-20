5ea70ffaa9ce719c02c9e7b192f847f52f3e45dd
MityaAndreevich <dmitry.logachev@icloud.com>
2021-08-20 15:49:02 +0300

    Changed constraints to dynamic, added override function for correct circles on different devices, corrected code for correct changing lights

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -2,10 +2,14 @@
 //  ViewController.swift
 //  TrafficLights
 //
-//  Created by Дмитрий Логачёв on 17.08.2021.
+//  Created by Dmitry Logachev on 17.08.2021.
 //
 
 import UIKit
+
+enum CurrentLights {
+    case red, yellow, green
+}
 
 class ViewController: UIViewController {
     
@@ -16,7 +20,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var startButton: UIButton!
     
+    var lights = CurrentLights.red
+    
     //MARK: - Override methods
+    override func viewDidLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.height / 2
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,40 +40,32 @@ class ViewController: UIViewController {
         yellowLight.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
         greenLight.backgroundColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
         
-        redLight.layer.cornerRadius = 60
-        yellowLight.layer.cornerRadius = 60
-        greenLight.layer.cornerRadius = 60
-        
         startButton.layer.cornerRadius = 10
-       
     }
-
     //MARK: - IB Actions
     @IBAction func showLightsStartButton() {
         
         startButton.setTitle("Next", for: .normal)
         
-        enum Colors {
-            case red, yellow, green
-        }
-        var colors = Colors.red
-        colors = .red
         
-        switch colors {
+        switch lights {
         case .red:
             redLight.alpha = 1
             yellowLight.alpha = 0.3
             greenLight.alpha = 0.3
+            lights = .yellow
     
         case .yellow:
             redLight.alpha = 0.3
             yellowLight.alpha = 1
             greenLight.alpha = 0.3
+            lights = .green
             
         case .green:
             redLight.alpha = 0.3
             yellowLight.alpha = 0.3
             greenLight.alpha = 1
+            lights = .red
            
         }
         
